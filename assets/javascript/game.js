d7209e6e01160f2f528768c13ad3abe4d598c21c
Mercyfulsin <aruiz858@gmail.com>
2019-07-06 21:06:04 -0700

    Made logo button that plays main theme. Song changes at different stages of the game. Changed text color.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //GLOBAL VARIABLES
-var guesses, wins, losses, remainder, wordList, answer, alphabetArr, htmlArr, answerRemainder;
+var guesses, wins, losses, remainder, wordList, answer, alphabetArr, htmlArr, answerRemainder, audioElement;
 alphabetArr = ("abcdefghijklmnopqrstuvwxyz1234567890").split("");
 wordList = ["ANAKIN", "DARTH VADER", "LUKE", "OBI-WAN", "C-3PO", "R2-D2", "CHEWBACCA", "HAN", "LEIA", "PADME", "QUI-GON", "EMPEROR", "YODA", "JAR JAR", "LANDO", "RED LEADER", "WEDGE", "BAIL ORGANA", "CAPTAIN PANAKA", "MACE WINDU", "NUTE GUNRAY", "BIGGS", "GOLD LEADER", "GENERAL GRIEVOUS", "TARKIN", "PIETT", "FODE/BEED", "RIC OLIE", "SHMI", "RUNE", "BOBA FETT", "OWEN", "ADMIRAL ACKBAR", "WATTO", "LAMA SU", "COUNT DOOKU", "GOLD FIVE", "RED TEN", "SIO BIBBLE", "JABBA", "MAD MADDA", "TAUN WE", "RIEEKAN", "CAPTAIN TYPHO", "ZEV", "DARTH MAUL", "BRAVO TWO", "JANGO FETT", "BOSS NASS", "GENERAL CEEL", "TEY HOW", "TC-14", "KITSTER", "RUWEE", "CLONE COMMANDER CODY", "CLONE COMMANDER GREE", "KI-ADI-MUNDI", "ODD BALL", "MON MOTHMA", "BERU", "DACK", "JANSON", "SEBULBA", "JIRA", "VALORUM", "WALD", "BRAVO THREE", "DOFINE", "TARPALS", "PK-4", "GREEDO", "CLIEGG", "SENATOR ASK AAK", "JOBAL", "ORN FREE TAA", "POGGLE", "SOLA", "SUN RIT", "PLO KOON", "FANG ZAR", "TION MEDON", "ON MEDON", "CAPTAIN ANTILLES", "GIDDEAN DANU", "MOTTI", "CAMIE", "DODONNA", "DERLIN", "OZZEL", "NEEDA", "BIB FORTUNA", "JERJERROD", "BOUSHH", "RABE", "REY", "POE", "SNAP", "GENERAL HUX", "SNOKE", "COLONEL DATOO", "KYLO REN", "JESS", "ELLO ASTY", "NIV LEK", "YOLO ZIFF", "FINN", "ADMIRAL STATURA", "CAPTAIN PHASMA", "MAZ", "BALA-TIK", "LIEUTENANT MITAKA", "UNKAR PLUTT", "LOR SAN TEKKA", "BB-8"];
 answer = wordList[Math.floor(Math.random() * Math.floor(wordList.length))].toLowerCase();
@@ -15,6 +15,8 @@ answerRemainder.forEach(function(content,index,obj){
 wins = 0;
 losses = 0;
 remainder = 9;
+audioElement = document.createElement("audio");
+audioElement.setAttribute("src", "assets/sounds/theme.ogg");
 
 //HTML Variables
 var hWins, hLosses, hRemainder, hGuesses, hAnswer, hWordArea, hUnderscoreArea,toastText;
@@ -29,9 +31,12 @@ toastText = document.getElementsByClassName("toast-body");
 generateHTML();
 
 $(document).ready(function(){
-    $(".toast").toast();
     $("#myToast").on('hidden.bs.toast', function(){
         window.location.reload();
+    });
+    $("#sw-logo").on("click", function(){
+        console.log("hello");
+        audioElement.play();
     });
 });
 
@@ -76,9 +81,17 @@ function updateStats() {
         alphabetArr = [];
         toastText[0].innerHTML = "You won!\nClosing this notification will refresh the page.";
         $('.toast').toast('show');
+    }else if(remainder === 1){
+        audioElement.pause();
+        audioElement.setAttribute("src", "assets/sounds/SW-Duel_Of_The_Fates.ogg");
+        audioElement.load();
+        audioElement.play();
     }else if(remainder <= 0){
-        console.log("test");
         alphabetArr = [];
+        audioElement.pause();
+        audioElement.setAttribute("src", "assets/sounds/chosen_one.ogg");
+        audioElement.load();
+        audioElement.play();
         toastText[0].innerHTML = "You lost!\nClosing this notification will refresh the page.";
         $('.toast').toast('show');
     }
